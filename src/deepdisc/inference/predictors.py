4a872fdc236c4bb5909fb6a2eeb686239b9c94d5
grantmerz <grantmerz97@gmail.com>
2025-06-19 13:47:53 -0500

    make sure notebooks run

--- src/deepdisc/inference/predictors.py
+++ src/deepdisc/inference/predictors.py
@@ -1,4 +1,16 @@
 import deepdisc.astrodet.astrodet as toolkit
+import copy
+import time 
+import torch
+import gc
+from detectron2.engine.defaults import create_ddp_model
+from detectron2.config import CfgNode, LazyConfig, get_cfg, instantiate
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.modeling import build_model
+from detectron2.data import DatasetCatalog, MetadataCatalog
+import detectron2.data.transforms as T
+import detectron2.utils.comm as comm
+
 
 
 class AstroPredictor:
